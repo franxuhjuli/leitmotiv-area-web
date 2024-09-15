19bc62c772458cc35f1598e7d26e5a1ccbd850eb
franxuhjuli <fjalvarez1984@gmail.com>
2024-09-15 15:50:10 +0200

    correciones version mobile 2

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,15 +2,15 @@ import { Button } from "./ui/button.jsx"
 export function Hero() {
   return (
     <section
-      className="bg-gradient-to-r from-[#228e70] to-[#63527c] text-white py-16 px-16
+      className="bg-gradient-to-r from-[#228e70] to-[#63527c] text-white py-8 px-8 md:py-16 md:px-16
     flex flex-col-reverse lg:flex-row items-center "
     >
       <div className="container mx-auto lg:px-20 text-center lg:text-left lg:w-2/3">
-        <h1 className="text-5xl  font-black mb-6">
+        <h1 className="text-5xl font-black mb-6">
           LEITMOTIV{" "}
           <span className="text-slate-800">AREA</span>
         </h1>
-        <p className="text-xl md:text-3xl text-bold  mb-4 md:mb-8">
+        <p className="text-xl md:text-3xl text-bold  mb-6 md:mb-8">
           "Cultura que une, movimientos creativos que generan comunidad e
           impulsan el cambio"
         </p>
